8e250725aeb50c0f187f3165feffa0bf8e84db4c
faizanalijutt0786-maker <faizanalijutt0786@gmail.com>
2026-06-24 15:55:13 +0500

    Configure GitHub Pages deployment

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -19,16 +19,13 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://faizanalijutt0786-maker.github.io',
+  baseUrl: '/computer-science-11-year/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'faizanalijutt0786-maker',
+  projectName: 'computer-science-11-year',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
@@ -97,7 +94,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/faizanalijutt0786-maker/computer-science-11-year',
             label: 'GitHub',
             position: 'right',
           },
